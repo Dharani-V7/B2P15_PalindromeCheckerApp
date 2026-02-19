63ad30a30fa595831f998c5966e7b28e22d82bcd
Dharani-V7 <dv5589@srmist.edu.in>
2026-02-19 11:07:05 +0530

    UC8:Hardcoded palindrome

---
+++ src/UC8PalindromeCheckerApp.java
@@ -0,0 +1,92 @@
+public class UC8PalindromeCheckerApp {
+
+    // Singly Linked List Node
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        // Convert string to linked list
+        Node head = createLinkedList(input);
+
+        // Palindrome check
+        if (isPalindrome(head)) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+
+    // Create linked list from string
+    private static Node createLinkedList(String str) {
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < str.length(); i++) {
+            Node newNode = new Node(str.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
+    // Check palindrome using fast & slow pointers
+    private static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverseList(slow.next);
+
+        // Compare both halves
+        Node firstHalf = head;
+        Node temp = secondHalf;
+
+        while (temp != null) {
+            if (firstHalf.data != temp.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+        return true;
+    }
+
+    // Reverse linked list
+    private static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
