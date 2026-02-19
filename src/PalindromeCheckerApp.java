313440a4ae0e0283036a3066a54db23c67a6e35a
Dharani-V7 <dv5589@srmist.edu.in>
2026-02-19 09:43:14 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,13 @@
 public class PalindromeCheckerApp {
 
+    // Entry point of the application
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println(" Application Version: 1.0 ");
+        System.out.println("====================================");
+
+        // Program ends here (UC1 completed)
+    }
 }
