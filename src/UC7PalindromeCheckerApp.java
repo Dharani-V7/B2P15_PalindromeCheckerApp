462cac464f4acff2219fc1cc48a36b0b73799aba
Dharani-V7 <dv5589@srmist.edu.in>
2026-02-19 11:02:13 +0530

    UC7:Hardcoded palindrome

---
+++ src/UC7PalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UC7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String input = "refer";
+
+        // Create Deque (Double Ended Queue)
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Remove and compare front and rear characters
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
